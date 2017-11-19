88ed1285419ad0fb4e0e73e218c193dca715c602
Chris Wozniak <chwoznia@cisco.com>
2017-11-18 17:41:22 -0700

    started the fragments

---
+++ fragments/_nav.php
@@ -0,0 +1,3 @@
+<div class="w3-bar w3-blue">
+   <a href="../public/home.html" class="w3-bar-item w3-button">Home</a>
+</div>
---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php 
+  echo '<!DOCTYPE html><html>';
+
+  require '../fragments/_head.php';
+
+  echo    '<body><div class="w3-container">';
+  
+  require '../fragments/_nav.php';
+  require '../fragments/_company.php'; 
+  require '../fragments/_footer.php';
+  
+  echo '</div></body></html>';
+?>

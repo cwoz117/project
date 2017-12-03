93f9a6463229e2d057d1e49c64c1931dec896b44
vagrant <ben.lbx@gmail.com>
2017-12-03 12:30:02 +0000

    transfered code from profile.php to _driver.php :

--- library/fragments/profile/_driver.php
+++ library/fragments/profile/_driver.php
@@ -4,12 +4,40 @@
       <h2>Add a Truck</h2>
     </div>
     <form class="w3-container w3-card-4 w3-padding" action="action/add_a_truck.php" method="post">
-      <label class="w3-text-grey w3-padding-small">Name</label>
-      <input type="text" class="w3-input w3-border"></input>  
       <label class="w3-text-grey w3-padding-small">Registration</label>
-      <input type="text" class="w3-input w3-border"></input>  
+      <input type="text" class="w3-input w3-border" name="reg">  
       <label class="w3-text-grey w3-padding-small">Insurance</label>
-      <input type="text" class="w3-input w3-border"></input>  
+      <input type="text" class="w3-input w3-border" name="insurance"> 
+      <label class="w3-text-grey w3-padding-small">Provider</label>
+      <input type="text" class="w3-input w3-border" name="provider">  
+      <label class="w3-text-grey w3-padding-small">Policy Number</label>
+      <input type="text" class="w3-input w3-border" name="polNum">  
+      <label class="w3-text-grey w3-padding-small">Plate Number</label>
+      <input type="text" class="w3-input w3-border" name="plate"> 
+      <label class="w3-text-grey w3-padding-small">Make</label>
+      <input type="text" class="w3-input w3-border" name="make">  
+      <label class="w3-text-grey w3-padding-small">Model</label>
+      <input type="text" class="w3-input w3-border" name="model">  
+      <label class="w3-text-grey w3-padding-small">Trailer Type</label>
+      <input type="text" class="w3-input w3-border" name="trailer" maxlength=2> 
+      <label class="w3-text-grey w3-padding-small">Year</label>
+      <input type="number" class="w3-input w3-border" name="year" max=99>  
+      <label class="w3-text-grey w3-padding-small">Province</label>
+      <select class="w3-input w3-border" name="prov"> 
+         <option value="AB">Alberta</option>
+         <option value="BC">British Columbia</option>  
+         <option value="MB">Manitoba</option>
+         <option value="NB">New Brunswick</option>  
+         <option value="NL">Newfoundland</option>
+         <option value="NS">Nova Scotia</option>  
+         <option value="NT">Northwest Territories</option>
+         <option value="NU">Nunavut</option>  
+         <option value="ON">Ontario</option>
+         <option value="PE">Prince Edward Island</option>  
+         <option value="QC">Quebec</option>
+         <option value="SK">Sasketchewan</option>
+         <option value="YT">Yukon</option>  
+      </select> 
       <span>
       <input class="w3-button w3-margin-top w3-blue w3-round w3-align-left" type="submit" value="Add">
       <a href="profile.php" class="w3-button w3-margin-top w3-red w3-round w3-align-right">Cancel</a>

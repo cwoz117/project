43960602eb4e7a976ee6c9c5a1a63c12a98950df
vagrant <ben.lbx@gmail.com>
2017-12-03 16:11:08 +0000

    Fixed Adding of trucks

--- public/action/add_a_truck.php
+++ public/action/add_a_truck.php
@@ -1,6 +1,7 @@
 <?php
+session_start();
 
-$userID = $_SESSIONS['userID'];
+$userID = $_SESSION['userID'];
 $prov = $_POST['provider'];
 $reg = $_POST['reg'];
 $ins = $_POST['insurance'];
@@ -10,17 +11,28 @@ $plate = $_POST['plate'];
 $make = $_POST['make'];
 $model = $_POST['model'];
 $trailer = $_POST['trailer'];
-$year = $_POST['year'];
+$year =  $_POST['year'];
+
+$conID;
 
 require("global/db.php");
 
-#How do I insert a year value???
-$sql = "insert into Truck(registration, contractor_id, provider, policy_num, plate_num, make, model, year, province, trailer_type) values ('$reg', $userID, '$prov', '$polNum', '$plate', '$make', '$model', '$year', '$prov', '$trailer');";
+echo "This is userID '$userID'.<br>";
+
+$sql = "select contractor_id from Driver where '$userID' = user_id;";
+
+$conID = (int) $link->query($sql);
+echo $conID;
+
+	
+
+$sql = "insert into Truck(registration, contractor_id, provider, policy_num, plate_num, make, model, year, province, trailer_type) values ('$reg', $conID, '$prov', '$polNum', '$plate', '$make', '$model', '$year', '$prov', '$trailer');";
 
 if ($link->query($sql) === true) {
 	echo "Success";
 } else {
-	echo "Failure";
+	echo "FAILED value returned: ";
+	echo $year;
 }
 
 $link->close();

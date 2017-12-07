c26eb11f428d18e7780a9d344c7262faa12e2925
Chris Wozniak <chwoznia@cisco.com>
2017-12-07 13:27:29 -0700

    updated market view

--- public/action/fill_market.php
+++ public/action/fill_market.php
@@ -1,7 +1,14 @@
 <?php
 
 function get_market_details(){
-  $sql = "select * from Workorder JOIN Company where user_id = company_id and completed = false;";
+  $sql = "select * 
+          from Workorder 
+          natural join Company
+          natural join Payload 
+          where user_id = company_id and completed = false and not exists(
+            select distinct workorder_no
+            from AcceptedOrders
+          );";
   require('global/db.php');
   $result = $link->query($sql);
   
@@ -16,7 +23,7 @@ function print_rows($result, $link){
   $i = 1;
   while ($row = $result->fetch_assoc()){
     make_button($i, $row['name'], $row['start_time'], $row['deadline'], $row['contract_price']);
-    make_details($i);
+    make_details($i, $row['pickup_address'],$row['dropoff_address'],$row['asset_value'],$row['cargo_type'],$row['gross_weight'],$row['contact_info']);
     $i++;
   }
 }
@@ -25,15 +32,16 @@ function make_button($i, $name, $start, $end, $price){
     echo "<button id='b$i' onclick='myFunction($i);' ";
     echo "class='w3-btn w3-block w3-left-align w3-round w3-border w3-white'>";
     echo "<table class='w3-table w3-small'><tr>";
-    echo array_map("make_data_rows", func_get_args());
+    foreach (array_map("make_data_rows", func_get_args()) as $row)
+      echo $row;
     echo "</tr></table></button>";
 }
 
-function make_details($i){
+function make_details($i, $pickup, $destination, $asset, $cargo, $weight, $contact){
   echo "<div id='$i' class='w3-container w3-hide'>";
   echo "  <div class='w3-container w3-border w3-padding w3-white'>";
-  echo "    <p>Pickup Address, Destination Address, asset_value, cargo type, gross weight, contact info</p>";
-  echo "    <button class='w3-btn'>Accept Contract</button></div></div>";
+  echo "    <p>$pickup, $destination, $asset, $cargo, $weight</p><p>$contact</p>";
+  echo "    <button class='w3-btn w3-round w3-blue'>Accept Contract</button></div></div>";
 }
 
 function make_data_rows($record){
